a290a96e7cf2b6a7129d58e2b9d087666589879a
tlholo-intern <tlhologelo.intern@bitcube.tech>
2023-02-01 11:08:43 +0200

    logo sizing and alt text

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "./images/pa-logo.png";
 
 function App() {
+  //shows today's date
+  let date = new Date().toUTCString().slice(5, 7);
+  //getting the name of the month
+  let monthName = new Date().toLocaleString('en-us', {month: 'long'});
+  //use this to get the current day of the week's name
+  let weekday = new Date().toLocaleString('en-us', {weekday: 'long'});
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
+      <div className="logo">
+        <div className="logo-image">
+          <img src={logo} alt="app logo"></img>
+        </div>
+      </div>
+      <div className="date-text-group">
+        <div className="date-text">
+          <h4>{date + " " + monthName +", " + weekday}</h4>
+        </div>
+        <div className="today-text">
+          <h1>Today</h1>
+        </div>
+      </div>
     </div>
   );
 }
